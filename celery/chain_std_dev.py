66e68fea258f7856b643674afa65b9e0cea372bb
Cloud User <ec2-user@ip-10-0-15-46.eu-west-2.compute.internal>
2024-04-20 13:34:04 +0000

    Clean unuse folders

--- celery/chain_std_dev.py
+++
@@ -1,8 +0,0 @@
-from tasks import power, mean, diff_vect, sq_vect
-from celery import signature, chain
-
-ages = [32,111,138,28,59,77,97]
-sq_rt_partial = power.s(0.5)  
-# result = chain( mean.s(ages), diff_vect.s(ages), sq_vect.s(), mean.s())()
-result = chain( mean.s(ages), diff_vect.s(ages), sq_vect.s(), mean.s(), sq_rt_partial)()
-print(result.get())
--- serving_ml/app.py
+++ serving_ml/app.py
@@ -64,21 +64,3 @@ def get_result(task_id):
     elif result.state == 'FAILURE':
         return jsonify({'state': result.state, 'status': str(result.info)}), 500
     return jsonify({'state': result.state, 'result': result.result}), 200
-
-# @app.post('/churn/predict', response_model=Task, status_code=202)
-# async def churn(customer: Customer):
-    
-#     task_id = predict_churn_single.delay(dict(customer))
-#     return {'task_id': str(task_id), 'status': 'Processing'}
-
-
-# @app.get('/churn/result/{task_id}', response_model=Prediction, status_code=200,
-#          responses={202: {'model': Task, 'description': 'Accepted: Not Ready'}})
-# async def churn_result(task_id):
-#     """Fetch result for given task_id"""
-#     task = AsyncResult(task_id)
-#     if not task.ready():
-#         print(app.url_path_for('churn'))
-#         return JSONResponse(status_code=202, content={'task_id': str(task_id), 'status': 'Processing'})
-#     result = task.get()
-#     return {'task_id': task_id, 'status': 'Success', 'probability': str(result)}
